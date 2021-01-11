388ae56264fa457eeded942b5cc26a575b772b8e
prem <premplayz@gmail.com>
2021-01-11 12:15:15 +0700

    Add project files.

---
+++ Assignment 1 Login System/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment_1_Login_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User bob = new User("Bobby","bob123","NotBob");
+            Console.WriteLine("User logged in:" + bob.loggedIn);
+            Console.WriteLine(bob.CheckLogin("Adam", "adam123"));
+            Console.WriteLine(bob.CheckLogin("Bobby", "bob123"));
+            Console.WriteLine("User logged in:" + bob.loggedIn);
+
+            Console.WriteLine(bob.RealName);
+
+            bob.LogOut();
+            Console.WriteLine("User Logged in:" + bob.loggedIn);
+
+            bob.ChangePassword("IAMBOB");
+            Console.WriteLine(bob.CheckLogin("Bobby","IAMBOB"));
+
+            
+            
+        }
+    }
+}
---
+++ Assignment 1 Login System/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1_Login_System
+{
+    class User
+    {
+        private string username;
+        private string password;
+        public string RealName;
+        public bool loggedIn = false; 
+        public User(string un, string pw , string Rn) // Constructor
+        {
+            username = un;
+            password = pw;
+            RealName = Rn;
+            
+        }
+
+        public bool CheckLogin(string un, string pw) // Function to check Username Password 
+        {
+            if (un == username && pw == password)
+            {
+                loggedIn = true;
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+                
+        }
+        public void LogOut()
+        {
+            loggedIn = false;
+        }
+
+        public void ChangePassword(string newPassword)
+        {
+            if(loggedIn == true)
+            {
+                Console.WriteLine("PASSWORD CHANGED");
+                password = newPassword;
+            }
+            else
+            {
+                Console.WriteLine("NOT LOGGED IN");
+            }
+        }
+    }
+}
